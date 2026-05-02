a82e02d08d4c614850258d3ca47d48a1c334ea3f
Nityanandayya Mathpathi <nithinimts@gmail.com>
2026-05-03 02:22:39 +0530

    fix: wrap DateFilter and DataGrid in Suspense

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,27 @@
+// export const dynamic = "force-dynamic";
+// import { DataGrid } from '../../components/data-grid';
+// import { DataCharts } from '../../components/data-charts';
+
+// export default function DashboardPage() {
+//   return (
+//     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+//         <DataGrid />
+//         <DataCharts />
+//     </div>
+//   );
+// };
+
 export const dynamic = "force-dynamic";
+import { Suspense } from "react";
 import { DataGrid } from '../../components/data-grid';
 import { DataCharts } from '../../components/data-charts';
 
 export default function DashboardPage() {
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
-        <DataGrid />
+        <Suspense fallback={<div>Loading...</div>}>
+          <DataGrid />
+        </Suspense>
         <DataCharts />
     </div>
   );
--- components/filters.tsx
+++ components/filters.tsx
@@ -1,9 +1,21 @@
+// import { DateFilter } from '@/components/date-filter';
+
+// export const Filters = () => {
+//     return (
+//         <div className="flex flex-col lg:flex-row items-center gap-y-2 lg:gap-y-0 lg:gap-x-2">
+//             <DateFilter />
+//         </div>
+//     );
+// };
+import { Suspense } from "react";
 import { DateFilter } from '@/components/date-filter';
 
 export const Filters = () => {
     return (
         <div className="flex flex-col lg:flex-row items-center gap-y-2 lg:gap-y-0 lg:gap-x-2">
-            <DateFilter />
+            <Suspense fallback={<div />}>
+                <DateFilter />
+            </Suspense>
         </div>
     );
 };
